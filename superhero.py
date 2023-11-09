e663ed68bab4f823108ba1f1cd12fa8a9e8716fd
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-11-08 16:46:33 -0800

    created new hero

--- superhero.py
+++ superhero.py
@@ -1,11 +1,25 @@
-class Hero:
+import random
+class hero:
     def __init__(self, name, starting_health=100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+    
+    def fight(self,opponent):   
+     heroes = [self, opponent]
+
+     winner_display = random.randint (0,1)
+     winner = heroes[winner_display]
+     loser = heroes[1 - winner_display]
+     print(f"{winner.name} defeated {loser.name}! {winner.name} Wins!")
 
 if __name__ == "__main__":
-        my_hero = Hero("Luminaflare", 200)
+        hero1 = hero("Luminaflare", 250)
+        hero2 = hero("Bolt", 200)
 
-        print(my_hero.name)
-        print(my_hero.current_health)
+        print(hero1.name)
+        print(hero1.current_health)
+        print(hero2.name)
+        print(hero2.current_health)
+
+        hero1.fight(hero2)
