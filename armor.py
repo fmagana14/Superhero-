61b8ae163c02a8bc7b0c4ba605456215ff4f7813
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-11-09 00:06:38 -0800

    added more to heroes

--- armor.py
+++ armor.py
@@ -1,5 +1,5 @@
 import random
-class armor:
+class Armor:
     def __init__(self, name, max_block):
         self.name = name
         self.max_block = max_block
@@ -9,6 +9,6 @@ class armor:
         return random_value
     
 if __name__ == "__main__":
-        my_armor = armor("debugging shields", 10)
+        my_armor = Armor("debugging shields", 10)
         print(my_armor.name)
         print(my_armor.block())
